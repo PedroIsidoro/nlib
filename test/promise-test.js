bb57b4f5a6aceb7b7274682fd983fc0affbffd8e
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-01 02:24:40 +0200

    Added Promise with tests

---
+++ test/promise-test.js
@@ -0,0 +1,46 @@
+var assert = require('assert'),
+    vows = require('vows'),
+    Promise = require('../lib/promise');
+
+
+vows.describe('Promise').addBatch({
+  "Promise instance": {
+    topic: new Promise(),
+    "response resolve() and done() methods": function (p) {
+      assert.isFunction(p.resolve);
+      assert.isFunction(p.done);
+    }
+  }
+}).addBatch({
+  "When promise resolved": {
+    topic: function () {
+      Promise(this.callback).done(this.callback).resolve(null, 123);
+    },
+    "it fires subscribed callbacks": function (err, result) {
+      assert.equal(result, 123);
+    }
+  }
+}).addBatch({
+  "When subscriber appended after promise was resolved": {
+    topic: function () {
+      Promise().resolve(null, 123).done(this.callback);
+    },
+    "it is fired as well": function (err, result) {
+      assert.equal(result, 123);
+    }
+  }
+}).addBatch({
+  "Trying to resolve promise more than once": {
+    topic: function () {
+      Promise().resolve(null, 123).resolve(null, 321).done(this.callback);
+    },
+    "does nothing": function (err, result) {
+      assert.equal(result, 123);
+    }
+  }
+}).export(module);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// vim:ts=2:sw=2
+////////////////////////////////////////////////////////////////////////////////
