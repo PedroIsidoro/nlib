ccd964bfcca932b7c73cd59c392cbe102e0cb8a0
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-19 03:26:19 +0200

    Added PriorityStack class

---
+++ test/priority_stack-test.js
@@ -0,0 +1,39 @@
+var assert = require('assert'),
+    vows = require('vows'),
+    Stack = require('../lib/nodeca-lib/priority_stack');
+
+
+vows.describe('PriorityStack').addBatch({
+  "When unshifting an object": {
+    topic: function () {
+      return Stack().unshift(0, 'c').unshift(0, 'b').unshift(0, 'a').flatten();
+    },
+    "given object is prepended into the priority sequence": function (arr) {
+      assert.equal(arr.length, 3);
+      assert.equal(arr.join(''), 'abc');
+    }
+  },
+  "When pushing an object": {
+    topic: function () {
+      return Stack().push(0, 'a').push(0, 'b').push(0, 'c').flatten();
+    },
+    "given object is prepended into the priority sequence": function (arr) {
+      assert.equal(arr.length, 3);
+      assert.equal(arr.join(''), 'abc');
+    }
+  },
+  "When stack is flatten": {
+    topic: function () {
+      return Stack().push(3, 'c').push(2, 'b').push(1, 'a').flatten();
+    },
+    "sequences are linked with respet of their priorities": function (arr) {
+      assert.equal(arr.length, 3);
+      assert.equal(arr.join(''), 'abc');
+    }
+  }
+}).export(module);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// vim:ts=2:sw=2
+////////////////////////////////////////////////////////////////////////////////
