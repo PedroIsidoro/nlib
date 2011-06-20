96c7cb3aad5d9667885f4376d13f7a3978c4c35e
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-20 23:44:54 +0200

    Removed PriorityStack#unshift

--- test/application-priority_stack-test.js
+++ test/application-priority_stack-test.js
@@ -4,15 +4,6 @@ var assert = require('assert'),
 
 
 vows.describe('Application.PriorityStack').addBatch({
-  "When unshifting an object": {
-    topic: function () {
-      return Stack().unshift(0, 'c').unshift(0, 'b').unshift(0, 'a').flatten();
-    },
-    "given object is prepended into the priority sequence": function (arr) {
-      assert.length(arr, 3);
-      assert.equal(arr.join(''), 'abc');
-    }
-  },
   "When pushing an object": {
     topic: function () {
       return Stack().push(0, 'a').push(0, 'b').push(0, 'c').flatten();
