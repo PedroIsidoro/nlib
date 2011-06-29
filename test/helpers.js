17cd07026cfe3ef6c0620401563557f00a2e5d35
Aleksey V Zapparov <ixti@member.fsf.org>
2011-06-29 17:17:34 +0200

    Improved MIME

--- test/helpers.js
+++ test/helpers.js
@@ -15,6 +15,37 @@ exports.buildStringManglersTest = function (callback, pairs) {
   return tests;
 };
 
+
+var mime_tests = function mime_tests(mimes, verb, callback) {
+  var tests = {};
+
+  Object.getOwnPropertyNames(mimes).forEach(function (type) {
+    var ext = mimes[type];
+    tests[verb + ' ' + ext.toUpperCase()] = function () {
+      callback(type, ext);
+    }
+  });
+
+  return tests;
+};
+
+
+exports.knowMimes = function (mime, map) {
+  return mime_tests(map, 'knows about', function (type, ext) {
+    assert.equal(mime.extension(type), ext);
+    assert.equal(mime.type('foobar.' + ext), type);
+  });
+};
+
+
+exports.knowNoMimes = function (mime, map) {
+  return mime_tests(map, 'knows nothing about', function (type, ext) {
+    assert.isUndefined(mime.extension(type));
+    assert.isUndefined(mime.type('foobar.' + ext));
+  });
+};
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // vim:ts=2:sw=2
 ////////////////////////////////////////////////////////////////////////////////
--- test/mime-test.js
+++ test/mime-test.js
@@ -1,19 +1,57 @@
 var assert = require('assert'),
     vows = require('vows'),
-    mime = require('../lib/nodeca-lib/mime');
+    helpers = require('./helpers'),
+    Mime = require('../lib/nodeca-lib/mime');
+
+
+var well_known_mimes = {
+  'application/javascript':         'js',
+  'application/x-shockwave-flash':  'swf',
+  'application/octet-stream':       'bin',
+
+  'audio/ogg':                      'ogg',
+  'audio/midi':                     'mid',
+  'audio/mpeg':                     'mp3',
+  'audio/webm':                     'weba',
+  'audio/x-ms-wma':                 'wma',
+  'audio/x-wav':                    'wav',
+
+  'image/gif':                      'gif',
+  'image/x-icon':                   'ico',
+  'image/jpeg':                     'jpg',
+  'image/png':                      'png',
+  'image/svg+xml':                  'svg',
+  'image/tiff':                     'tiff',
+
+  'text/plain':                     'txt',
+  'text/css':                       'css',
+  'text/html':                      'html',
+
+  'video/x-flv':                    'flv',
+  'video/ogg':                      'ogv',
+  'video/quicktime':                'mov',
+  'video/mpeg':                     'mpg',
+  'video/x-msvideo':                'avi',
+  'video/x-ms-wmv':                 'wmv',
+  'video/webm':                     'webm'
+};
 
 
 vows.describe('Mime').addBatch({
-  "When getting type": {
-    "for unknown extension Undefined is returned": function () {
+  "Type of unknown extension": {
+    topic: new Mime,
+    "is undefined": function (mime) {
       assert.isUndefined(mime.type('filename.unknownExtension'));
     }
   },
-  "When getting extension": {
-    "for unknown mime-type Undefined is returned": function () {
+  "Extension of unknown type": {
+    topic: new Mime,
+    "is undefined": function (mime) {
       assert.isUndefined(mime.extension('foobar/bazbaz'));
     }
-  }
+  },
+  "New instance by default": helpers.knowNoMimes(new Mime, well_known_mimes),
+  "Dummy instance": helpers.knowMimes(Mime, well_known_mimes)
 }).export(module);
 
 
