a04251cfce2f4e74d65bb5a76c94fe3ec8624bff
Aleksey V Zapparov <ixti@member.fsf.org>
2011-07-01 18:42:54 +0200

    Added StticManager path parser

--- test/static_manager-test.js
+++ test/static_manager-test.js
@@ -3,13 +3,34 @@ var assert = require('assert'),
     VFS = require('../lib/nodeca-lib/vfs'),
     StaticManager = require('../lib/nodeca-lib/static_manager');
 
+
+var testRealpathParser = function testRealpathParser(map) {
+  var tests = {};
+  
+  Object.getOwnPropertyNames(map).forEach(function (realpath) {
+    var path = map[realpath][0],
+        actn = map[realpath][1],
+        prio = map[realpath][2];
+
+    tests[realpath + ' -> ' + actn + ' [' + prio + '] ' + path] = function () {
+      var obj = StaticManager.parsePath(realpath);
+      assert.strictEqual(obj.path, path);
+      assert.strictEqual(obj.actn, actn);
+      assert.strictEqual(obj.prio, prio);
+    };
+  });
+
+  return tests;
+};
+
+
 vows.describe('StaticManager').addBatch({
   "compile()": {
     topic: function () {
       var sm = new StaticManager();
 
-      sm.addPath(__dirname + '/fixtures/a')
-        .addPath(__dirname + '/fixtures/b');
+      sm.addPath(__dirname + '/fixtures/static/a')
+        .addPath(__dirname + '/fixtures/static/b');
 
       return sm.compile();
     },
@@ -32,7 +53,18 @@ vows.describe('StaticManager').addBatch({
         '\s*/\* _app.css/baz.css \*/\s*'
       ));
     },
-  }
+  },
+  "When parsing file paths": testRealpathParser({
+    'app.css':                ['app.css', 'override', null],
+    'foobar/app.css':         ['foobar/app.css', 'override', null],
+    'app.js.patch':           ['app.js', 'patch', null],
+    'app.js.99.after':        ['app.js', 'after', 99],
+    'app.js.99.before':       ['app.js', 'before', 99],
+    '_app.styl/abc':          ['app.styl', 'after', null],
+    'foobar/_app.styl/abc':   ['foobar/app.styl', 'after', null],
+    'foo/_bar/_app.styl/abc': ['foo/_bar/app.styl', 'after', null],
+    'foobar/_app.styl':       ['foobar/_app.styl', 'override', null]
+  })
 }).export(module);
 
 
